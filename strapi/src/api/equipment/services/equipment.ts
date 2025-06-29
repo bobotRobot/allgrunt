cf41fba03c47e08d0472a78d61ccd5197d3e3d1c
bobotRobot <71061857+bobotRobot@users.noreply.github.com>
2025-06-30 00:16:06 +0300

    Исправление схемы Equipment в Strapi

--- strapi/src/api/equipment/services/equipment.ts
+++ strapi/src/api/equipment/services/equipment.ts
@@ -1,3 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::equipment-item.equipment-item');
+export default factories.createCoreService('api::equipment.equipment');
